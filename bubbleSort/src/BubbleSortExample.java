8424a77788c80b9d535ed62d4b33a3727478e1cd
Tom Mustermann <Tom.Mustermann@example.com>
2025-07-12 08:14:38 +0200

    Rename parameter arr to intArray in bubbleSort for clarity

--- bubbleSort/src/BubbleSortExample.java
+++ bubbleSort/src/BubbleSortExample.java
@@ -1,14 +1,14 @@
 public class BubbleSortExample {
-        static void bubbleSort(int[] arr) {
-            int n = arr.length;
+        static void bubbleSort(int[] intArray) {
+            int n = intArray.length;
             int temp = 0;
             for(int i=0; i < n; i++){
                 for(int j=1; j < (n-i); j++){
-                    if(arr[j-1] > arr[j]){
+                    if(intArray[j-1] > intArray[j]){
                         //swap elements
-                        temp = arr[j-1];
-                        arr[j-1] = arr[j];
-                        arr[j] = temp;
+                        temp = intArray[j-1];
+                        intArray[j-1] = intArray[j];
+                        intArray[j] = temp;
                     }
                 }
             }
