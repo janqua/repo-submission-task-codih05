16fca13c4f835714629986a198f1758022bcd590
anna mustermann <anna.mustermann@example.com>
2025-07-12 08:19:52 +0200

    Add main program entry point to run bubbleSort Add early-exit check in bubbleSort if array is already sorted

--- bubbleSort/src/BubbleSortExample.java
+++ bubbleSort/src/BubbleSortExample.java
@@ -1,16 +1,26 @@
+import java.util.Arrays;
+
 public class BubbleSortExample {
         static void bubbleSort(int[] arr) {
+
+            System.out.println("Before bubble sort : "+Arrays.toString(arr));
             int n = arr.length;
             int temp = 0;
+            boolean noChange;
+
             for(int i=0; i < n; i++){
+                noChange= true;
                 for(int j=1; j < (n-i); j++){
                     if(arr[j-1] > arr[j]){
                         //swap elements
+                        noChange= false;
                         temp = arr[j-1];
                         arr[j-1] = arr[j];
                         arr[j] = temp;
                     }
                 }
+                if(noChange) break;
             }
+            System.out.println("After bubble sort : "+ Arrays.toString(arr));
         }
 }
